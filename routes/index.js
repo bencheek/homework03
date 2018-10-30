1397bb40f674c19ec4bac9847f8a8d7db3605033
mbencik <mbencik@casenetllc.com>
2018-10-30 04:16:24 -0400

    Authorization

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,22 @@
 
 const Router = require('koa-router')
 const { handleErrors, handleNotFound } = require('../middleware/errors')
+const users = require('../controllers/users')
 const dogsController = require('./../controllers/dogs')
+const { authenticate } = require('./../middleware/authentication')
 
 const router = new Router()
 
 router.use(handleErrors)
 
+router.post('/users', users.signUp)
+
 router
-  .get('/dog', dogsController.list)
-  .get('/dog/:id', dogsController.read)
-  .post('/dog', dogsController.create)
-  .put('/dog', dogsController.update)
-  .delete('/dog/:id', dogsController.remove)
+  .get('/dog', authenticate, dogsController.list)
+  .get('/dog/:id', authenticate, dogsController.read)
+  .post('/dog', authenticate, dogsController.create)
+  .put('/dog', authenticate, dogsController.update)
+  .delete('/dog/:id', authenticate, dogsController.remove)
 
 router.use(handleNotFound)
 
