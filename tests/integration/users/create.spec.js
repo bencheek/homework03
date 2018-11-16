c9ed26f52a665ed018a1fe499873c2e408465d54
mbencik <mbencik@casenetllc.com>
2018-11-16 07:09:03 -0500

    Separating tests to standalone cases

--- tests/integration/users/create.spec.js
+++ tests/integration/users/create.spec.js
@@ -5,6 +5,7 @@ const { expect } = require('chai')
 const app = require('../../../app')
 const { resetDb } = require('../../helpers')
 const usersRepository = require('../../../repositories/users')
+const crypto = require('../../../utils/crypto')
 
 describe('Users', () => {
   beforeEach(resetDb)
@@ -30,6 +31,7 @@ describe('Users', () => {
         id: 1,
       })
 
+
       expect(signUpResponse.body).to.have.all.keys([
         'name',
         'email',
@@ -38,6 +40,14 @@ describe('Users', () => {
         'id',
         'accessToken',
       ])
+    })
+
+    it('logs in existing user and allows access protected URL', async () => {
+      await usersRepository.create({
+        ...userData,
+        disabled: false,
+        password: await crypto.hashPassword('11111111'),
+      })
 
       // log in correct user and password
       const logInResponse = await request(app)
@@ -69,20 +79,26 @@ describe('Users', () => {
         .set('Authorization', `${logInResponse.body.accessToken}`)
         .send()
         .expect(200)
+    })
 
-      // try to access GET /dog without JWT token
+    it('denies user to access protected URL without JWT token', async () => {
+      // try to access GET /dog without JWT token,
       await request(app)
         .get('/dog')
         .send()
         .expect(401)
+    })
 
+    it('denies user to access protected URL with invalid JWT token', async () => {
       // try to access GET /dog with wrong JWT token
       await request(app)
         .get('/dog')
         .set('Authorization', 'XXX.XXX.XXX')
         .send()
         .expect(401)
+    })
 
+    it('denies nonexisting user to log in', async () => {
       // log in nonexistent user
       await request(app)
         .post('/sessions/user')
@@ -91,6 +107,15 @@ describe('Users', () => {
           password: '11111111',
         })
         .expect(401)
+    })
+
+    it('denies user with invalid password to log in', async () => {
+
+      await usersRepository.create({
+        ...userData,
+        disabled: false,
+        password: await crypto.hashPassword('11111111'),
+      })
 
       // log in user with incorrect password
       await request(app)
